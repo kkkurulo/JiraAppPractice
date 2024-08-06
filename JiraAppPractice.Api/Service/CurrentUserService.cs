482695a63646ad34416a81d3591b0b6559851e34
Kyrylo Chornopyskyi <chornopyskyikurulo@gmail.com>
2024-08-06 14:18:40 +0300

    Update user service.

---
+++ JiraAppPractice.Api/Service/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using JiraAppPractice.Services.Interfaces;
+using System.Security.Claims;
+
+namespace ToDoApp.Api.Service
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly HttpContext _context;
+
+        public CurrentUserService(IHttpContextAccessor accessor)
+        {
+            _context = accessor.HttpContext!;
+        }
+
+        public string UserId => _context!.User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+    }
+}
